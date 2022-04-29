dfe81255a61b51decb54e11b0b0c68230d9bb2af
fortunatos <30715191+fortunatos@users.noreply.github.com>
2022-04-29 01:41:28 -0300

    Add files via upload

---
+++ scriptrdn.js
@@ -0,0 +1,13 @@
+function AbreLink1()     
+{
+  
+window.location.href = "https://t.acam.link/133199/153/3100?source="+rastreio2;
+  
+}
+  
+function AbreLink2()      
+{
+  
+window.location.href = "https://t.acam.link/133199/153/3098?source="+rastreio2; 
+  
+} 
