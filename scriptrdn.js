c8f8a3e9a7b234fd63cdae18a13776ba76a8901b
fortunatos <30715191+fortunatos@users.noreply.github.com>
2022-04-29 01:49:45 -0300

    Add files via upload

---
+++ scriptrdn.js
@@ -0,0 +1,192 @@
+
+function AbreLink1()     
+{
+  
+window.location.href = "https://t.acam.link/133199/153/3100?source="+rastreio2;
+  
+}
+  
+function AbreLink2()      
+{
+  
+window.location.href = "https://t.acam.link/133199/153/3098?source="+rastreio2; 
+  
+} 
+
+
+$.getScript("http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js", function(){
+   alert("Script loaded but not necessarily executed.");
+});
+
+$.getScript("https://unpkg.com/sweetalert2@7.12.15/dist/sweetalert2.all.js", function(){
+   alert("Script loaded but not necessarily executed.");
+});
+
+var language = navigator.language || navigator.userLanguage; 
+language = language.substring( 0, 2 );
+  
+  
+jQuery(document).ready(function(){
+if (document.cookie.indexOf(&#39;visited=true&#39;) == -1) {
+var fifteenDays = 1000*60*60*36;
+var expires = new Date((new Date()).valueOf() + fifteenDays);
+document.cookie = &quot;visited=true;expires=&quot; + expires.toUTCString();
+
+if  (language == &quot;pt&quot;){
+         
+swal({
+  html: &#39;<h4>Para assistir bucetas ao vivo na cam, basta se cadastrar e confirmar seu email</h4>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continuar &#39;
+  });
+}
+        
+else if (language == &quot;es&quot;){
+    
+swal({
+  
+  html: &#39;<h3>Regístrese para ver el coño en vivo en la cámara</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continuar &#39;
+  });    
+} 
+  
+else if (language == &quot;fr&quot;){
+  
+  
+swal({
+  
+  html: &#39;<h3>Pour voir la chatte en direct à la caméra, il suffit de vous inscrire et de confirmer votre e-mail</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continuer &#39;
+  });
+}  
+  
+  
+  else if (language == &quot;it&quot;){
+  
+  
+swal({
+
+  html: &#39;<h3>Per vedere la figa dal vivo sulla telecamera, basta registrarsi e confermare la tua email</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continua &#39;
+  }); 
+}  
+  
+    else if (language == &quot;de&quot;){
+  
+  
+swal({
+  
+  html: &#39;<h3>Um Live-Muschi vor der Kamera zu sehen, registriere dich einfach und bestätige deine E-Mail</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Fortsetzen &#39;
+  });
+}  
+  
+      else if (language == &quot;en&quot;){
+  
+  
+swal({
+
+  html: &#39;<h3>To watch pussy live on cam, just register and confirm your email</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continue &#39;
+  });
+}  
+
+else {
+      
+swal({
+
+  html: &#39;<h3>To watch pussy live on cam, just register and confirm your email.</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continue &#39;
+  });
+  
+}
+  
+setTimeout(&#39;AbreLink1()&#39;, 5000);  
+  
+}else{
+  
+if  (language == &quot;pt&quot;){
+         
+swal({
+  html: &#39;<h4>Compre moedas e de gorjeta para sua camgirl favorita.</h4>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continuar &#39;
+  });
+}
+        
+else if (language == &quot;es&quot;){
+    
+swal({
+  
+  html: &#39;<h3>Compra monedas y dale propina a tu camgirl favorita.</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continuar &#39;
+  });    
+} 
+  
+else if (language == &quot;fr&quot;){
+  
+  
+swal({
+  
+  html: &#39;<h3>Achetez des pièces et donnez un pourboire à votre camgirl préférée.</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continuer &#39;
+  });
+}  
+  
+  
+  else if (language == &quot;it&quot;){
+  
+  
+swal({
+
+  html: &#39;<h3>Acquista monete e dai la mancia alla tua camgirl preferita.</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continua &#39;
+  }); 
+}  
+  
+    else if (language == &quot;de&quot;){
+  
+  
+swal({
+  
+  html: &#39;<h3>Kaufen Sie Münzen und geben Sie Ihrem Lieblings-Camgirl Trinkgeld.</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Fortsetzen &#39;
+  });
+}  
+  
+      else if (language == &quot;en&quot;){
+  
+  
+swal({
+
+  html: &#39;<h3>Buy coins and tip your favorite camgirl.</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continue &#39;
+  });
+}  
+
+else {
+      
+swal({
+
+  html: &#39;<h3>Buy coins and tip your favorite camgirl.</h3>&#39;,
+  confirmButtonColor: &#39;#3085d6&#39;,
+  confirmButtonText: &#39; Continue &#39;
+  });
+}  
+  
+setTimeout(&#39;AbreLink2()&#39;, 5000);
+  
+}
+});  
+   
